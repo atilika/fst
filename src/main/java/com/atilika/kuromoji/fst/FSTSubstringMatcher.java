863153ef97b3b8077e6bf6042d0befff3f785176
Yoshinari Fujinuma <fujinumay@gmail.com>
2015-04-08 20:42:48 +0900

    Added simple tests for VM-based substring matcher

--- src/main/java/com/atilika/kuromoji/fst/FSTSubstringMatcher.java
+++ src/main/java/com/atilika/kuromoji/fst/FSTSubstringMatcher.java
@@ -1,18 +1,34 @@
 package com.atilika.kuromoji.fst;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FSTSubstringMatcher {
+    private String sentence;
 
-    // method to match all possible substrings
-    public void matchAllSubstrings(String word, VirtualMachine vm, VirtualMachine.Program program) {
-        for (int i = 0; i < word.length(); i++) {
-            for (int j = i + 1; j < word.length(); j++) {
-                if (vm.run(program, word.substring(i, j)) != -1) {
-                    System.out.println(word.substring(i, j));
-                    // current implementation does not fail immediately
+    FSTSubstringMatcher(String sentence) {
+        this.sentence = sentence;
+    }
+
+    /**
+     * Matching all possible substrings
+     *
+     * @param vm
+     * @param program
+     * @return List of extracted tokens that are in a dictionary
+     */
+    public List matchAllSubstrings(VirtualMachine vm, VirtualMachine.Program program) {
+        List extractedTokens = new ArrayList();
+
+        for (int i = 0; i < sentence.length(); i++) {
+            for (int j = i + 1; j < sentence.length(); j++) {
+                if (vm.run(program, sentence.substring(i, j)) != -1) {
+//                    System.out.println(sentence.substring(i, j));
+                    extractedTokens.add(sentence.substring(i, j));
                 }
             }
         }
+
+        return extractedTokens;
     }
 }
--- src/test/java/com/atilika/kuromoji/fst/FSTSubstringMatcherTest.java
+++ src/test/java/com/atilika/kuromoji/fst/FSTSubstringMatcherTest.java
@@ -2,42 +2,56 @@ package com.atilika.kuromoji.fst;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class FSTSubstringMatcherTest {
     // We want to test how fast the lookup method is.
-
     // input: wikipedia article
     // output: all of the entries in the dictionary
 
-
     @Test
-    public void testExtractOneWord() throws Exception {
+    public void testExtractTwoTokens() throws Exception {
         String sampleSentence = "寿司が食べたい"; // "I want to eat sushi." in Japanese
 
-//        List<String> words = new ArrayList<>();
-//        words.add("寿司");
-//        words.add("食べ");
-
-        String[] words = {"寿司", "食べ"};
+        String[] tokens = {"寿司", "食べ"};
         int[] outputValues = {1, 2};
 
         FST fst = new FST();
-        fst.createDictionary(words, outputValues);
+        fst.createDictionary(tokens, outputValues);
 
         VirtualMachine vm = new VirtualMachine();
         VirtualMachine.Program program = new VirtualMachine.Program();
         program.addInstructions(fst.fstCompiler.instructionList);
 
-        for (int i = 0; i < words.length; i++) {
-            assertEquals(outputValues[i], vm.run(program, words[i]));
-        }
-        assertEquals(-1, vm.run(program, "まぐろ"));
+        FSTSubstringMatcher fstSubstringMatcher = new FSTSubstringMatcher(sampleSentence);
+        List extractedTokens = fstSubstringMatcher.matchAllSubstrings(vm, program);
 
-        FSTSubstringMatcher fstSubstringMatcher = new FSTSubstringMatcher();
+        assertEquals(Arrays.asList(tokens), extractedTokens);
+    }
 
-        fstSubstringMatcher.matchAllSubstrings(sampleSentence, vm, program);
+    @Test
+    public void testExtractLongSentence() throws Exception {
+        String sampleSentence = "寿司（すし、鮨、鮓[注釈 1]）と呼ばれる食品は、酢飯と主に魚介類を組み合わせた日本料理である。" +
+                "大別すると、生鮮魚介を用いた「早鮨（早ずし）」と、魚介類を飯と塩で乳酸発酵させた「なれ鮨（なれずし）」に区分される。" +
+                "そのなかでも代表的な寿司は前者の握り寿司（江戸前寿司）であり、英語圏では“sushi”で通じる料理となっている。" +
+                "（詳細は、各々「江戸前寿司」「なれずし」を参照）"; // Explanation of sushi in Japanese
 
+        String[] tokens = {"寿司", "食べ"};
+        int[] outputValues = {1, 2};
 
+        FST fst = new FST();
+        fst.createDictionary(tokens, outputValues);
+
+        VirtualMachine vm = new VirtualMachine();
+        VirtualMachine.Program program = new VirtualMachine.Program();
+        program.addInstructions(fst.fstCompiler.instructionList);
+
+        FSTSubstringMatcher fstSubstringMatcher = new FSTSubstringMatcher(sampleSentence);
+        List extractedTokens = fstSubstringMatcher.matchAllSubstrings(vm, program);
+        String[] expectedTokens = {"寿司", "寿司", "寿司", "寿司", "寿司"};
+        assertEquals(Arrays.asList(expectedTokens), extractedTokens);
     }
 }
